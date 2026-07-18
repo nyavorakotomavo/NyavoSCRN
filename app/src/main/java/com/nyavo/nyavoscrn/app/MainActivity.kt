b426b4ab321b4387e6d83421c7d56c84f90559be
nyavorakotomavo6-max <nyavorakotomavo6@gmail.com>
2026-07-18 14:58:54 +0300

    fix: correct MainActivity.kt imports

--- app/src/main/java/com/nyavo/nyavoscrn/app/MainActivity.kt
+++ app/src/main/java/com/nyavo/nyavoscrn/app/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialThemeimport androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

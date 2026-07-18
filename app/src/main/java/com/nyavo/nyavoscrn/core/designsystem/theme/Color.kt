1b5e7ce5579bf64bc7ac967a035d0a5c07596474
nyavorakotomavo <nyavosapp@gmail.com>
2026-07-18 17:54:26 +0300

    Update Color.kt

--- app/src/main/java/com/nyavo/nyavoscrn/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/nyavo/nyavoscrn/core/designsystem/theme/Color.kt
@@ -6,22 +6,17 @@ val Violet900 = Color(0xFF0A0014)
 val Violet800 = Color(0xFF1A0033)
 val Violet700 = Color(0xFF2D004D)
 val Violet600 = Color(0xFF400066)
-val Violet500 = Color(0xFF530080)
+val Violet500 = Color(0xFF530080) // Nuance 1 : écran qui marche (bleu-violet)
 val Violet400 = Color(0xFF660099)
-val Violet300 = Color(0xFF8000B3)
+val Violet300 = Color(0xFF8000B3) // Nuance 2 : faux touchers
 val Violet200 = Color(0xFF9900CC)
 val Violet100 = Color(0xFFB300E6)
-val Violet50 = Color(0xFFCC00FF)
+val Violet50 = Color(0xFFCC00FF)  // Zone active (violet lumineux)
 
-val PixelPalette = listOf(
-    Violet900, Violet800, Violet700, Violet600, Violet500,
-    Violet400, Violet300, Violet200, Violet100, Violet50
-)
-
-val ZoneWorkingColor = Violet500
-val ZoneFalsePositiveColor = Violet300
-val ZoneActiveGlow = Violet50
 val ZoneDeadColor = Color(0xFFE53935)
+val ZoneFalsePositiveColor = Violet300
+val ZoneWorkingColor = Violet500
+val ZoneActiveGlow = Violet50
 
 fun Color.lighten(factor: Float): Color {
     return Color(
